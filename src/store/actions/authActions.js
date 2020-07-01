dc7c8d22e80115bc7176d8d52adc8b18b095ce4b
Bryan <bwmaravilla_14@hotmail.com>
2020-07-01 15:00:39 -0400

    error handling added, when user does not provide a picture

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -30,27 +30,43 @@ export const signUp = (newUser) => {
     const storage = firebase.storage().ref();
     const firestore = getFirestore();
 
-    firebase.auth().createUserWithEmailAndPassword(
-      newUser.email, 
-      newUser.password        
-    ).then(resp => {
-      storage.child(`users/${new Date().getTime()}`).put(newUser.picture).then((snapshot) => {
-        storage.child(snapshot.metadata.fullPath).getDownloadURL().then((url) => {
+    if(newUser.firstName && newUser.lastName){
+      
+      firebase.auth().createUserWithEmailAndPassword(
+        newUser.email, 
+        newUser.password        
+      ).then(resp => {
+
+        if(newUser.picture){
+          storage.child(`users/${new Date().getTime()}`).put(newUser.picture).then((snapshot) => {
+            storage.child(snapshot.metadata.fullPath).getDownloadURL().then((url) => {
+              return firestore.collection('users').doc(resp.user.uid).set({
+                firstName: newUser.firstName,
+                lastName: newUser.lastName,
+                initials: newUser.firstName[0] + newUser.lastName[0],
+                employee: newUser.employee,
+                picture: url
+              });
+            })
+          }).catch((err) => {
+            console.log(err);
+          })
+        } else{
           return firestore.collection('users').doc(resp.user.uid).set({
             firstName: newUser.firstName,
             lastName: newUser.lastName,
             initials: newUser.firstName[0] + newUser.lastName[0],
             employee: newUser.employee,
-            picture: url
           });
-        })
+        }
+        
+      }).then(() => {
+        dispatch({ type: 'SIGNUP_SUCCESS' });
       }).catch((err) => {
-        console.log(err);
-      })
-    }).then(() => {
-      dispatch({ type: 'SIGNUP_SUCCESS' });
-    }).catch((err) => {
-      dispatch({ type: 'SIGNUP_ERROR', err});
-    });
+        dispatch({ type: 'SIGNUP_ERROR', err});
+      });
+    }
+    else
+      dispatch({ type: 'SIGNUP_ERROR', err: {message: 'Please provide name and/or lastname'}});
   }
 }
