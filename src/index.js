73b25585d62f52a25e9a5cd10c04c77ae7c793c7
Bryan <bwmaravilla_14@hotmail.com>
2020-07-08 23:28:08 -0400

    Modified to accomodate reactfire

--- src/index.js
+++ src/index.js
@@ -3,16 +3,29 @@ import ReactDOM from 'react-dom';
 import "./index.css";
 import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore, applyMiddleware, compose } from "redux";
-import rootReducer from "./store/reducers/rootReducer";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import { reduxFirestore, getFirestore, createFirestoreInstance } from 'redux-firestore';
-import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase';
+import { FirebaseAppProvider, SuspenseWithPerf } from 'reactfire';
 import firebaseConf from "./Firebase/firebase";
-import firebase from "firebase/app";
+// import { createStore, applyMiddleware, compose } from "redux";
+// import rootReducer from "./store/reducers/rootReducer";
+// import { Provider } from "react-redux";
+// import thunk from "redux-thunk";
+// import { reduxFirestore, getFirestore, createFirestoreInstance } from 'redux-firestore';
+// import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase';
+// import firebaseConf from "./Firebase/firebase";
+// import firebase from "firebase/app";
 
-const store = createStore(rootReducer,
+ReactDOM.render(
+  <BrowserRouter>
+    <FirebaseAppProvider firebaseConfig={firebaseConf} >
+      <SuspenseWithPerf fallback={<p>loading app...</p>} traceId={'loading-app'}>
+        <App />
+      </SuspenseWithPerf>
+    </FirebaseAppProvider>
+  </BrowserRouter>
+  , document.getElementById('root')
+);
+
+/*const store = createStore(rootReducer,
   compose(
     applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
     reduxFirestore(firebase, firebaseConf)
@@ -39,4 +52,4 @@ ReactDOM.render(
       </ReactReduxFirebaseProvider>
     </Provider>
   </BrowserRouter>, document.getElementById('root')
-);
+);*/
